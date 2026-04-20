6bf9e3d74563ea64761d88579231fb99e6a0ac53
chrisjo-zzz <jonathanhutajulu94@gmail.com>
2026-04-20 10:44:08 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -7,54 +7,56 @@
  */
 
 #include <stdio.h>
+#include <math.h>
 
-int main() {
+int main(){
     int n;
-    scanf("%d", &n);
-    int arr[n];
-    for(int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+    scanf ("%d", &n);
+    int array[n];
+    for (int i = 0; i < n; i++){
+        scanf ("%d", &array[i]);
     }
 
-    for(int i = 0; i < n; i++) {
-        if(arr[i] == -1) {
+    for (int i = 0; i < n; i++){
+        if(array[i] == -1){
             int left = -1, right = -1;
-            for(int j = i - 1; j >= 0; j--) {
-                if(arr[j] != -1) {
-                    left = arr[j];
+            for (int j = i - 1; j >= 0; j--){
+                if(array[j] != -1){
+                    left = array[j];
                     break;
                 }
             }
-            for(int j = i + 1; j < n; j++) {
-                if(arr[j] != -1) {
-                    right = arr[j];
+            for (int j = i + 1; j < n; j++){
+                if(array[j] != -1){
+                    right = array[j];
                     break;
                 }
             }
-            if(left != -1 && right != -1) {
-                arr[i] = (left + right) / 2;
-            } else if(left != -1) {
-                arr[i] = left;
-            } else if(right != -1) {
-                arr[i] = right;
-            } else {
-                arr[i] = 0;
+            if (left != -1 && right != -1){
+                double num = (left + right)/2;
+                if (num < 0){
+                    if (left % 2 == 0 && right % 2 ==0 || left % 2 != 0 && right % 2 !=0){
+                    array[i] = num;
+                    } else{
+                    array[i] = num -1;
+                    }
+                }       
+            } else if (left != -1){
+                array[i] = left;
+            } else if (right != -1){
+                array[i] = right;
+            } else{
+                array[i] = 0;
             }
         }
     }
 
-    int max_sum = arr[0];
-    int cur = arr[0];
-    for(int i = 1; i < n; i++) {
-        cur = (arr[i] > cur + arr[i]) ? arr[i] : cur + arr[i];
-        max_sum = (max_sum > cur) ? max_sum : cur;
-    }
-
+    int jumlah_max = 0;
     printf("RECOVERED");
-    for(int i = 0; i < n; i++) {
-        printf(" %d", arr[i]);
+    for(int i = 0; i < n; i++){
+        printf(" %d", array[i]);
+        jumlah_max += array[i];
     }
-    printf("\nMAX_SUM %d\n", max_sum);
-
+    printf("\nMAX_SUM %d\n", jumlah_max);
     return 0;
 }
