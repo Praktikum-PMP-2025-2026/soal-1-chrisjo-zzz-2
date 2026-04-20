3b9c2ed4027f78b0aeb7fa18f11174ba44f16ab2
chrisjo-zzz <jonathanhutajulu94@gmail.com>
2026-04-20 09:36:48 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -1,5 +1,60 @@
+/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2024/2025
+ *   Modul               : 2
+ *   Hari dan Tanggal    : 20 April 2026
+ *   Nama (NIM)          : Christian Jonathan Hutajulu (13224108)
+ *   Nama File           : Soal1.c
+ *   Deskripsi           : Program pemulihan array lalu menghitung jumlah subarray maksimum
+ */
+
 #include <stdio.h>
 
-int main(void) {
+int main(){
+    int n;
+    scanf ("%d", &n);
+    int array[n];
+    for (int i = 0; i < n; i++){
+        scanf ("%d", &array[i]);
+    }
+
+    for (int i = 0; i < n; i++){
+        if(array[i] == -1){
+            int left = -1, right = -1;
+            for (int j = i - 1; j >= 0; j--){
+                if(array[j] != -1){
+                    left = array[j];
+                    break;
+                }
+            }
+            for (int j = i + 1; j < n; j++){
+                if(array[j] != -1){
+                    right = array[j];
+                    break;
+                }
+            }
+            if (left != -1 && right != -1){
+                array[i] = (left + right)/2;
+            } else if (left != -1){
+                array[i] = left;
+            } else if (right != -1){
+                array[i] = right;
+            } else{
+                array[i] = 0;
+            }
+        }
+    }
+
+/*    int jumlah_max = array[0];
+    int x = array[0];
+    for (int i = 0; i < n; i++){
+        x = (array[i] > x + array[i]) ? array[i] : x + array[i];
+        jumlah_max = (jumlah_max > x) ? jumlah_max : x;
+    }*/
+    int jumlah_max = 0;
+    printf("RECOVERED");
+    for(int i = 0; i < n; i++){
+        printf(" %d", array[i]);
+        jumlah_max += array[i];
+    }
+    printf("\nMAX_SUM %d\n", jumlah_max);
     return 0;
 }
